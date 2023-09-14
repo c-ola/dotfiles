21ce5bcf7772ca174e946706014703a70cd9a20b
Nikola Zupancic <nikola.z37@hotmail.com>
2023-09-14 18:52:26 -0400

    idk

---
+++ nvim/nvim/lua/cola-nvim/init.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = " "
+require("cola-nvim.theme")
+require("cola-nvim.set")
+require("cola-nvim.remap")
+print("Hello Nikola")
+
--- update.sh
+++ update.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 # This script is meant to sync all of my local dotfiles to this git repo
+# It can also sync the local files with the contents of the repo (use at own risk) also cant do it yet
 
-cp -r ~/.config/nvim ./nvim
+#a1=$(wc -l < $1)
+
+cp -r $HOME/.config/{nvim,alacritty} .
+
+#cp -r $HOME/.config/allacritty/{themes/} .
+
+cp -r $HOME/.config/xournalpp/{colornames.ini,palette.gpl,settings.xml} .
+
+cp $HOME/{.zshrc,.p10k.zsh,.profile} .
+    
