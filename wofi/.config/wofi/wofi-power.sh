88a10d1655d35576849e870fb2f3ed764121338c
Nikola Zupancic <nikola.z37@hotmail.com>
2025-11-08 05:00:05 -0500

    add wofi

---
+++ wofi/.config/wofi/wofi-power.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+entries="⇠ Logout\n⏾ Suspend\n⭮ Reboot\n⏻ Shutdown"
+
+selected=$(echo -e $entries|wofi --conf ~/.config/wofi/powermenuconf --style ~/.config/wofi/style.css --dmenu --cache-file /dev/null | awk '{print tolower($2)}')
+
+case $selected in
+  logout)
+    hyprctl dispatch exit;;
+  suspend)
+    exec systemctl suspend;;
+  reboot)
+    exec systemctl reboot;;
+  shutdown)
+    exec systemctl poweroff -i;;
+esac
